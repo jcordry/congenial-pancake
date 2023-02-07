d3f9bdace0fd6b5baa0eee3477765b53a0f439b1
Julien Cordry <u0026939@linux2.scedt.tees.ac.uk>
2023-02-07 15:21:51 +0000

    first commit

---
+++ app/src/main/java/uk/ac/tees/scedt/mad/u0026939/asherscollection/MainActivity.java
@@ -0,0 +1,14 @@
+package uk.ac.tees.scedt.mad.u0026939.asherscollection;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
